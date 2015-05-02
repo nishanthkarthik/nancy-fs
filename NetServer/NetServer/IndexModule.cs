9cbc2b68939aa1d6c2596f4225e1a9f9359fee6e
Karthik Nishanth Sengottuvelavan <nishanthkarthik@live.com>
2015-05-03 00:38:14 +0530

    Initial creation

---
+++ NetServer/NetServer/IndexModule.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Nancy.Responses;
+
+namespace NetServer
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = (parameters =>
+            {
+                return View["index"];
+            });
+
+            Get["/furmark"] = (parameters =>
+            {
+                GenericFileResponse genericFileResponse = new GenericFileResponse(@);
+            });
+        }
+    }
+}
