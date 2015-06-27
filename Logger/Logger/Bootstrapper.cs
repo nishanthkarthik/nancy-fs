5e461ef28304a6ce2fc807796c4233b1fb74c0c2
Karthik Nishanth Sengottuvelavan <nishanthkarthik@live.com>
2015-06-27 10:27:02 +0530

    Logger

--- Logger/Logger/Bootstrapper.cs
+++ Logger/Logger/Bootstrapper.cs
@@ -1,4 +1,4 @@
-﻿namespace SimpleServer
+﻿namespace Logger
 {
     using Nancy;
 
--- Logger/Logger/Properties/AssemblyInfo.cs
+++ Logger/Logger/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleServer")]
+[assembly: AssemblyTitle("Logger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleServer")]
+[assembly: AssemblyProduct("Logger")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
